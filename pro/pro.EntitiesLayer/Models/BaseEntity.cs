af2a756682ffa503aba92637eeccff028ccdcc92
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-06-13 13:09:48 +0300

    sub category table is created

---
+++ pro/pro.EntitiesLayer/Models/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro.EntitiesLayer.Models
+{
+   public  class BaseEntity
+    {
+        public DateTime CreatedDate  { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public DateTime RemovedDate { get; set; }
+
+    }
+}
---
+++ pro/pro.EntitiesLayer/Models/SubCategory.cs
@@ -0,0 +1,26 @@
+﻿using pro.CoreLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro.EntitiesLayer.Models
+{
+    public class SubCategory:IEntity
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SubCategoryId { get; set; }
+
+        public int CategoryId { get; set; } // categoryId FK
+
+        public String Name { get; set; }
+
+       
+        // Her alt kategorinin bir kategorisi vardır 
+
+
+    }
+}
