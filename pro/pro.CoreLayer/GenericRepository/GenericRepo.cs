ae1af3040ec1f285a34a44353badb3fbab3504df
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-06-18 13:08:12 +0300

    generic repository ready

---
+++ pro/pro.CoreLayer/GenericRepository/GenericRepo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro.CoreLayer.GenericRepository
+{
+    public class GenericRepo<T> : IGenericRepository<T> where T : class, new()
+    {
+        protected DbContext _context;
+        private DbSet<T> objectSet;
+        public GenericRepo(DbContext context)
+        {
+            _context = context;
+            objectSet = _context.Set<T>();
+        }
+        
+        public void Add(T entity)
+        {
+            objectSet.Add(entity);
+
+        }
+
+        public void AddRange(List<T> entities)
+        {
+            objectSet.AddRange(entities);
+
+        }
+
+        public T Find(Expression<Func<T, bool>> where)
+        {
+            return objectSet.FirstOrDefault(where);
+        }
+
+        public List<T> FindAll(Expression<Func<T, bool>> predicate)
+        {
+            return objectSet.Where(predicate).ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            objectSet.Remove(entity);
+        }
+
+        public void RemoveRange(List<T> entities)
+        {
+            objectSet.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            // Save Change 
+            _context.Entry<T>(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ pro/pro.CoreLayer/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro.CoreLayer.GenericRepository
+{
+    public interface IGenericRepository<TEntity> where TEntity:class,new()
+    {
+        // ortak Kulanılan metodlar
+        void Add(TEntity entity);
+
+        void Remove(TEntity entity);
+
+        void Update(TEntity entity);
+
+        TEntity Find(Expression<Func<TEntity, bool>> where);
+
+        List<TEntity> FindAll(Expression<Func<TEntity, bool>> predicate);
+
+        void RemoveRange(List<TEntity> entities);
+
+        void AddRange(List<TEntity> entities);
+
+
+
+    }
+}
