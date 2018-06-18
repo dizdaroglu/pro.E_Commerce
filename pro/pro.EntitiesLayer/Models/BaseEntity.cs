0e3e586cba0b6c735de410ada45a6567647b8e6c
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-06-18 12:21:44 +0300

    base entity update

--- pro/pro.EntitiesLayer/Models/BaseEntity.cs
+++ pro/pro.EntitiesLayer/Models/BaseEntity.cs
@@ -8,7 +8,7 @@ namespace pro.EntitiesLayer.Models
 {
    public  class BaseEntity
     {
-
+        //
         public DateTime CreatedDate  { get; set; }
         public DateTime ModifiedDate { get; set; }
         public DateTime RemovedDate { get; set; }
